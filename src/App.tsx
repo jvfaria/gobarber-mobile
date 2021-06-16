2b1efbd37d8572d4d0a77bd2851290fdec66c4cb
João Vitor de Faria <jvfaria015@gmail.com>
2021-06-16 15:54:43 -0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { Text, View, StatusBar, StyleSheet } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './routes';
+
+import AppProvider from './hooks';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="default" />
-      <View style={{ flex: 1, backgroundColor: '#312E38' }}>
-        <Routes />
-      </View>
+      <StatusBar barStyle="light-content" backgroundColor="#312E38" />
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#312E38' }}>
+          <Routes />
+        </View>
+      </AppProvider>
     </NavigationContainer>
   );
 };

d4d581b6020e1d4d17aa8ff8e3b4ee54ab0ea923
João Vitor de Faria <jvfaria015@gmail.com>
2021-06-16 15:54:30 -0300

    signup

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,8 +1,162 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useCallback, useRef } from 'react';
+
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useNavigation } from '@react-navigation/native';
+
+import * as Yup from 'yup';
+
+import { Form } from '@unform/mobile';
+
+import { FormHandles } from '@unform/core';
+import {
+  Container,
+  Title,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+import api from '../../services/api';
+
+import Input from '../../components/Input';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import Button from '../../components/Button';
+
+interface UserProps {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  const formRef = useRef<FormHandles>(null);
+
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const handleSignUp = useCallback(
+    async (data: UserProps) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .email('Digite um email válido')
+            .required('Email obrigatório'),
+          password: Yup.string().min(6, 'Mínimo 6 dígitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        Alert.alert(
+          'Cadastro realizado com sucesso !',
+          'Agora você já pode fazer o Login na aplicação',
+        );
+        navigation.goBack();
+      } catch (error) {
+        // const errors = getValidationErrors(error);
+        // formRef.current?.setErrors(errors);
+
+        Alert.alert(
+          'Erro ao realizar o cadastro',
+          'Ocorreu o erro ao realizar o cadastro, tente novamente',
+        );
+      }
+    },
+    [navigation],
+  );
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Form
+              style={{ width: '100%' }}
+              ref={formRef}
+              onSubmit={handleSignUp}
+            >
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                name="email"
+                icon="mail"
+                placeholder="email"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="senha"
+                returnKeyType="send"
+                secureTextEntry
+              />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Criar conta
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+        <CreateAccountButton onPress={() => navigation.navigate('Login')}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <CreateAccountButtonText>Voltar para login</CreateAccountButtonText>
+        </CreateAccountButton>
+      </KeyboardAvoidingView>
+    </>
+  );
 };
 
 export default SignUp;
--- src/pages/Signup/styles.ts
+++ src/pages/Signup/styles.ts
@@ -1,3 +1,48 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  padding: 0 30px 100px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0;
+
+  flex: 1;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+`;
+export const CreateAccountButtonText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
+`;
